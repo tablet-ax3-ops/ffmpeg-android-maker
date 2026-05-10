e7300b1eea8139f9cd47ad345a5e8100c71592d5
tablet-ax3-ops <tablet.tester.user93@gmail.com>
2026-05-10 17:47:32 +0200

    Update build.sh

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -1,34 +1,38 @@
 #!/usr/bin/env bash
 
+# --- 1. OTTIMIZZAZIONI ARCHITETTURA (Focus su ARM NEON) ---
 case $ANDROID_ABI in
   x86)
-    # Disabling assembler optimizations, because they have text relocations
     EXTRA_BUILD_CONFIGURATION_FLAGS="$EXTRA_BUILD_CONFIGURATION_FLAGS --disable-asm"
     ;;
   x86_64)
     EXTRA_BUILD_CONFIGURATION_FLAGS="$EXTRA_BUILD_CONFIGURATION_FLAGS --x86asmexe=${NASM_EXECUTABLE}"
     ;;
+  armeabi-v7a)
+    # Forza NEON e ottimizzazioni per CPU a 32bit (VFPv3 è il minimo per performance decenti)
+    EXTRA_BUILD_CONFIGURATION_FLAGS="$EXTRA_BUILD_CONFIGURATION_FLAGS --enable-asm --enable-inline-asm --enable-neon"
+    EXTRA_CFLAGS="-mfloat-abi=softfp -mfpu=neon"
+    ;;
+  arm64-v8a)
+    # 64-bit: NEON è standard, ma abilitiamo l'ottimizzazione specifica per i registri estesi
+    EXTRA_BUILD_CONFIGURATION_FLAGS="$EXTRA_BUILD_CONFIGURATION_FLAGS --enable-asm --enable-inline-asm --enable-neon"
+    ;;
 esac
 
-if [ "$FFMPEG_GPL_ENABLED" = true ] ; then
-    EXTRA_BUILD_CONFIGURATION_FLAGS="$EXTRA_BUILD_CONFIGURATION_FLAGS --enable-gpl"
-fi
+# --- 2. CONFIGURAZIONE DEI COMPONENTI (Massima Dieta) ---
+# Disabilitiamo tutto e riaccendiamo solo il bisturi per GoPro
+ADDITIONAL_COMPONENTS="--disable-everything \
+  --enable-decoder=hevc \
+  --enable-parser=hevc \
+  --enable-demuxer=mov,mp4,hevc \
+  --enable-protocol=file,pipe"
 
-# Preparing flags for enabling requested libraries
-ADDITIONAL_COMPONENTS=
-for LIBARY_NAME in ${FFMPEG_EXTERNAL_LIBRARIES[@]}
-do
-  ADDITIONAL_COMPONENTS+=" --enable-$LIBARY_NAME"
-done
-
-# Referencing dependencies without pkgconfig
+# --- 3. PARAMETRI DI COMPILAZIONE ---
 DEP_CFLAGS="-I${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/include"
 DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
-
-# Android 15 with 16 kb page size support
-# https://developer.android.com/guide/practices/page-sizes#compile-r27
 EXTRA_LDFLAGS="-Wl,-z,max-page-size=16384 $DEP_LD_FLAGS"
 
+# Lancio della configurazione
 ./configure \
   --prefix=${BUILD_DIR_FFMPEG}/${ANDROID_ABI} \
   --enable-cross-compile \
@@ -43,15 +47,31 @@ EXTRA_LDFLAGS="-Wl,-z,max-page-size=16384 $DEP_LD_FLAGS"
   --nm=${FAM_NM} \
   --ranlib=${FAM_RANLIB} \
   --strip=${FAM_STRIP} \
-  --extra-cflags="-O3 -fPIC $DEP_CFLAGS" \
-  --extra-ldflags="$EXTRA_LDFLAGS" \
+  \
+  --extra-cflags="-O3 -fPIC -ffast-math -flto -funroll-loops $EXTRA_CFLAGS $DEP_CFLAGS" \
+  --extra-ldflags="-O3 -flto $EXTRA_LDFLAGS" \
+  \
   --enable-shared \
   --disable-static \
+  --disable-debug \
+  --disable-doc \
+  --disable-programs \
+  --disable-avdevice \
+  --disable-postproc \
+  --disable-avfilter \
+  --disable-network \
   --disable-vulkan \
-  --pkg-config=${PKG_CONFIG_EXECUTABLE} \
+  \
+  --enable-pthreads \
+  --enable-optimizations \
+  --enable-hardcoded-tables \
+  --enable-runtime-cpudetect \
+  \
+  ${ADDITIONAL_COMPONENTS} \
   ${EXTRA_BUILD_CONFIGURATION_FLAGS} \
-  $ADDITIONAL_COMPONENTS || exit 1
+  --pkg-config=${PKG_CONFIG_EXECUTABLE} || exit 1
 
+# Compilazione parallela sfruttando tutti i core della macchina (GitHub Actions ne ha solitamente 2 o 4)
 ${MAKE_EXECUTABLE} clean
 ${MAKE_EXECUTABLE} -j${HOST_NPROC}
 ${MAKE_EXECUTABLE} install
